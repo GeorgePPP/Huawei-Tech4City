eaad1b1615dca44ed4f58b8c485709e170947581
Lx <142951132+lexian24@users.noreply.github.com>
2024-06-05 20:29:25 +0800

    Updating python script and requirements

---
+++ whisper/whisper.py
@@ -0,0 +1,29 @@
+# !pip install --q git+https://github.com/m-bain/whisperx.git
+import whisperx
+import gc 
+
+device = "cuda" 
+audio_file = "sample.mp4"
+batch_size = 16
+compute_type = "float16"
+
+# ASR
+model = whisperx.load_model("large-v3", device, compute_type=compute_type)
+
+audio = whisperx.load_audio(audio_file)
+result = model.transcribe(audio, batch_size=batch_size)
+print(result["segments"]) 
+
+model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+
+print(result["segments"])
+
+#Diarization
+diarize_model = whisperx.DiarizationPipeline(use_auth_token='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', device=device)
+
+diarize_segments = diarize_model(audio)
+
+result = whisperx.assign_word_speakers(diarize_segments, result)
+print(diarize_segments)
+print(result["segments"])
