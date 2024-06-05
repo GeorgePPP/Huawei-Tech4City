66ae92bf433680ed8e4c6cfb1783c05b6a46d145
george <geor0014@e.ntu.edu.sg>
2024-06-05 19:58:55 +0800

    Create utils function backbone

--- app_exp.py
+++ app_exp.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from utils import generate_report, analyze_meeting, improve_body_language, generate_meeting_report_from_labels
 
 app = Flask(__name__)
 
---
+++ utils.py
@@ -0,0 +1,11 @@
+def generate_report():
+    pass
+
+def analyze_meeting():
+    pass
+
+def improve_body_language():
+    pass
+
+def generate_meeting_report_from_labels():
+    pass
