31ef1417a8e37b3bf0c25ff41f40b07ea607c31d
SiriusYH <64475165+SiriusYH@users.noreply.github.com>
2024-06-08 16:45:52 +0800

    Confidence - Body Language Completed
    Added several test videos

--- BodyLanguage/Body_language.py
+++ BodyLanguage/Body_language.py
@@ -1,7 +1,7 @@
-#this version y axis is flipped
 import cv2
 import mediapipe as mp
-import numpy as np
+import pandas as pd
+import os
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -18,13 +18,42 @@ def calculate_quadrilateral_area(point1, point2, point3, point4):
     triangle2_area = calculate_triangle_area(point1, point3, point4)
     return triangle1_area + triangle2_area
 
-cap = cv2.VideoCapture(0)
+def format_time(time_in_seconds):
+    hours = int(time_in_seconds // 3600)
+    minutes = int((time_in_seconds % 3600) // 60)
+    seconds = int(time_in_seconds % 60)
+    milliseconds = int((time_in_seconds % 1) * 1000)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+
+cap = cv2.VideoCapture('path/to/your/videos')
+
+if not cap.isOpened():
+    print("Error opening the video file.")
+    exit()
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+confidence_intervals = []
+current_confidence = False
+start_time = 0
+end_time = 0
+segment_count = 0
+
+output_dir = 'path/to/your/output'
+os.makedirs(output_dir, exist_ok=True)
+
+output_path = os.path.join(output_dir, 'output_video.mp4')
+out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
-            print("Ignoring empty camera frame.")
-            continue
+            break
+
+        current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
 
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -42,16 +71,20 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                 results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height]
             left_wrist_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x * image_width,
                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y * image_height]
-            right_wrist_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x * image_width,
-                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y * image_height]
+            right_wrist_coordinates = [
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x * image_width,
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y * image_height]
             left_elbow_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x * image_width,
                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y * image_height]
-            right_elbow_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x * image_width,
-                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y * image_height]
-            left_shoulder_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width,
-                                         results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height]
-            right_shoulder_coordinates = [results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * image_width,
-                                          results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y * image_height]
+            right_elbow_coordinates = [
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x * image_width,
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y * image_height]
+            left_shoulder_coordinates = [
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width,
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height]
+            right_shoulder_coordinates = [
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * image_width,
+                results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y * image_height]
 
             if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].visibility < 0.5 and \
                     results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].visibility < 0.5:
@@ -62,31 +95,66 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                     elbow_y_avg = sum(coord[1] for coord in visible_elbows) / len(visible_elbows)
                     shoulder_y_avg = sum(coord[1] for coord in visible_shoulders) / len(visible_shoulders)
 
-                    if elbow_y_avg <= shoulder_y_avg+5:
+                    if elbow_y_avg <= shoulder_y_avg + 5:
                         confidence = True
             else:
                 wrist_coordinates = [left_wrist_coordinates, right_wrist_coordinates]
                 shoulder_coordinates = [left_shoulder_coordinates, right_shoulder_coordinates]
-
+                elbow_coordinates = [left_elbow_coordinates, right_elbow_coordinates]
                 if any(wrist[1] < shoulder[1] for wrist in wrist_coordinates for shoulder in shoulder_coordinates):
                     confidence = True
                 else:
-                    triangle_area = calculate_triangle_area(nose_coordinates, left_wrist_coordinates, right_wrist_coordinates)
-                    quadrilateral_area = calculate_quadrilateral_area(left_shoulder_coordinates, right_shoulder_coordinates,
+                    triangle_area = calculate_triangle_area(nose_coordinates, left_wrist_coordinates,
+                                                            right_wrist_coordinates)
+                    quadrilateral_area = calculate_quadrilateral_area(left_shoulder_coordinates,
+                                                                      right_shoulder_coordinates,
                                                                       left_elbow_coordinates, right_elbow_coordinates)
-
-                    if triangle_area > quadrilateral_area:
+                    shoulder_triangle = calculate_triangle_area(nose_coordinates, left_shoulder_coordinates,
+                                                                right_shoulder_coordinates)
+                    if any(wrist[1] < elbow[1] for wrist in wrist_coordinates for elbow in elbow_coordinates):
+                        elbow_triangle = calculate_triangle_area(nose_coordinates, left_elbow_coordinates,
+                                                                 right_elbow_coordinates)
+                        if elbow_triangle > quadrilateral_area:
+                            confidence = True
+                    elif (shoulder_triangle + triangle_area) > quadrilateral_area:
                         confidence = True
 
-        # Display confidence status on the image
-        confidence_text = "Confidence: " + ("True" if confidence else "False")
+        if confidence != current_confidence:
+            if current_confidence is not None:
+                end_time = current_time
+                formatted_start_time = format_time(start_time)
+                formatted_end_time = format_time(end_time)
+                confidence_intervals.append({'segment': f"[{formatted_start_time} --> {formatted_end_time}]", 'start': start_time, 'end': end_time, 'label': str(current_confidence)})
+                segment_count += 1
+            start_time = current_time
+            current_confidence = confidence
+
+        if confidence:
+            confidence_text = "Confidence: True"
+        else:
+            confidence_text = "Confidence: False"
         cv2.putText(image, confidence_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # Flip the image horizontally for a selfie-view display.
-        # Display the image without flipping
+        out.write(image)
+
         cv2.imshow('MediaPipe Pose', image)
         if cv2.waitKey(5) == ord('q'):
             break
 
+    end_time = current_time
+    formatted_start_time = format_time(start_time)
+    formatted_end_time = format_time(end_time)
+    confidence_intervals.append({'segment': f"[{formatted_start_time} --> {formatted_end_time}]", 'start': start_time, 'end': end_time, 'label': str(current_confidence)})
+
 cap.release()
+out.release()
 cv2.destroyAllWindows()
+
+# Display confidence intervals as a DataFrame
+df = pd.DataFrame(confidence_intervals)
+df.columns = ['Segment', 'Start', 'End', 'Confidence']
+print(df)
+
+# Save confidence intervals as an Excel file
+excel_path = os.path.join(output_dir, 'confidence_intervals.xlsx')
+df.to_excel(excel_path, index=False)
